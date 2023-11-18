5095a511ca5920d3876d0d48a2129725bda1a703
CarterNR <carter090604@gmail.com>
2023-11-18 01:37:33 -0600

    conexion en teoria exitosa

--- Proyecto/prueba.py
+++ Proyecto/prueba.py
@@ -1,5 +1,9 @@
 import cx_Oracle
 import tkinter as tk
+cx_Oracle.init_oracle_client(lib_dir=r"D:\Oracle\instantclient_19_21")
+
+
+
 
 try:
     connection=cx_Oracle.connect(
@@ -9,7 +13,6 @@ try:
         encoding='UTF-8'
     )
     print(connection.version)
+    print("hola")
 except cx_Oracle.Error as error:
-        print(error)
-
-
+    print(error)
